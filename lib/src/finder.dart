16ff7160b254bade10b2f82db419c342559ea1a5
tejHackerDEV <tejasimha222@gmail.com>
2024-07-22 16:49:27 +0530

    feat: Add an function to find element by `text` its rendering

---
+++ lib/src/finder.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+
+class Finder {
+  final WidgetsBinding _binding;
+
+  const Finder(this._binding);
+
+  Element? _findByText(String text, Element? visitor) {
+    if (visitor == null) {
+      return null;
+    }
+    Element? result;
+    final renderObject = visitor.renderObject;
+    if (renderObject is RenderParagraph) {
+      final textToMatch = renderObject.text
+          .getSemanticsInformation()
+          .map((info) => info.text)
+          .join('');
+      if (textToMatch == text) {
+        result = visitor;
+      }
+    }
+    return result ?? _findByText(text, visitor);
+  }
+
+  /// Finds an [Element] if the `text` it is rendering
+  /// matched with the given [text].
+  ///
+  /// <br>
+  /// If no matches returns `null`.
+  Element? findByText(String text) {
+    return _findByText(text, _binding.rootElement);
+  }
+}
