218f11644029cf5f027448ca0caec83198c76de9
tejHackerDEV <tejasimha222@gmail.com>
2024-07-27 14:00:04 +0530

    refactor(flutterenium)!: Optimize the code in `FindAction` by resuing the common code across sub-classes
    Signed-off-by: tejHackerDEV <tejasimha222@gmail.com>

--- packages/flutterenium/lib/src/actions/find.dart
+++ packages/flutterenium/lib/src/actions/find.dart
@@ -15,24 +15,16 @@ sealed class FindAction extends Action {
     };
   }
 
-  Element? execute(WidgetsBinding binding);
-}
-
-class FindByLabelAction extends FindAction {
-  final String label;
-
-  const FindByLabelAction(this.label);
-
-  factory FindByLabelAction.fromJson(Map<String, dynamic> json) {
-    return FindByLabelAction(json['label']);
-  }
+  /// If this returns `true` then the [execute] method
+  /// will return the [element]
+  bool matcher(Element element);
 
   Element? _find(Element? visitor) {
     if (visitor == null) {
       return null;
     }
     Element? result;
-    if (visitor.widget.label == label) {
+    if (matcher(visitor)) {
       result = visitor;
     }
     if (result == null) {
@@ -43,79 +35,59 @@ class FindByLabelAction extends FindAction {
     }
     return result;
   }
+
+  Element? execute(WidgetsBinding binding) {
+    return _find(binding.rootElement);
+  }
+}
+
+class FindByLabelAction extends FindAction {
+  final String label;
 
   /// Finds an [Element] if the `label` it is assigned
   /// matched with the given [label].
   ///
   /// <br>
   /// If no matches returns `null`.
+  const FindByLabelAction(this.label);
+
+  factory FindByLabelAction.fromJson(Map<String, dynamic> json) {
+    return FindByLabelAction(json['label']);
+  }
+
   @override
-  Element? execute(WidgetsBinding binding) {
-    return _find(binding.rootElement);
+  bool matcher(Element element) {
+    return element.widget.label == label;
   }
 }
 
 class FindByTextAction extends FindAction {
   final String text;
 
+  /// Finds an [Element] if the `text` it is rendering
+  /// matched with the given [text].
+  ///
+  /// <br>
+  /// If no matches returns `null`.
   const FindByTextAction(this.text);
 
   factory FindByTextAction.fromJson(Map<String, dynamic> json) {
     return FindByTextAction(json['text']);
   }
 
-  Element? _find(Element? visitor) {
-    if (visitor == null) {
-      return null;
-    }
-    Element? result;
-    final renderObject = visitor.renderObject;
+  @override
+  bool matcher(Element element) {
+    bool matched = false;
+    final renderObject = element.renderObject;
     if (renderObject is RenderParagraph) {
       final textToMatch = renderObject.toPlainText();
-      if (textToMatch == text) {
-        result = visitor;
-      }
+      matched = textToMatch == text;
     }
-    if (result == null) {
-      // as result not found, recursively check for it
-      visitor.visitChildren((visitor) {
-        result ??= _find(visitor);
-      });
-    }
-    return result;
-  }
-
-  /// Finds an [Element] if the `text` it is rendering
-  /// matched with the given [text].
-  ///
-  /// <br>
-  /// If no matches returns `null`.
-  @override
-  Element? execute(WidgetsBinding binding) {
-    return _find(binding.rootElement);
+    return matched;
   }
 }
 
 class FindByWidget<T extends Widget> extends FindAction {
-  const FindByWidget();
-
-  Element? _find(Element? visitor) {
-    if (visitor == null) {
-      return null;
-    }
-    Element? result;
-    if (visitor.widget is T) {
-      result = visitor;
-    }
-    if (result == null) {
-      // as result not found, recursively check for it
-      visitor.visitChildren((visitor) {
-        result ??= _find(visitor);
-      });
-    }
-    return result;
-  }
-
   /// Finds an [Element] if the `widget` it is holding
   /// of type [T]. If [root] is `null` then it start
   /// looking for the widget from the rootElement of
@@ -124,6 +96,13 @@ class FindByWidget<T extends Widget> extends FindAction {
   ///
   /// <br>
   /// If no matches returns `null`.
+  const FindByWidget();
+
+  @override
+  bool matcher(Element element) {
+    return element.widget is T;
+  }
+
   @override
   Element? execute(WidgetsBinding binding, {Element? root}) {
     return _find(root ?? binding.rootElement);
