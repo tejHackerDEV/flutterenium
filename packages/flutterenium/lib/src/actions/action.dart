696bceb92dc569193f106145495c98ef2b30311a
tejHackerDEV <tejasimha222@gmail.com>
2024-07-28 20:31:38 +0530

    feat(flutterenium): Add `pump` & `pumpAndSettle` functions to wait between frames
    Signed-off-by: tejHackerDEV <tejasimha222@gmail.com>

--- packages/flutterenium/lib/src/actions/action.dart
+++ packages/flutterenium/lib/src/actions/action.dart
@@ -1,3 +1,8 @@
+import 'dart:async';
+
+import 'package:clock/clock.dart';
+import 'package:flutter/widgets.dart' hide ScrollAction;
+
 import 'find.dart';
 import 'get_text.dart';
 import 'scroll.dart';
@@ -17,5 +22,38 @@ abstract class Action {
       'is_visible' => const IsVisibleAction(),
       _ => throw UnimplementedError(),
     };
+  }
+
+  DateTime get currentDateTime => clock.now();
+
+  /// Waits till the end of the current frame completion
+  Future<void> pump(
+    WidgetsBinding binding, [
+    Duration duration = Duration.zero,
+  ]) async {
+    if (duration <= Duration.zero) {
+      await binding.endOfFrame;
+      return;
+    }
+    final waitTill = currentDateTime.add(duration);
+    while (currentDateTime.isBefore(waitTill)) {
+      await binding.endOfFrame;
+    }
+  }
+
+  /// Waits till all the scheduled frames get completed.
+  /// If the scheduled frames won't get completed
+  /// by the specified [duration], this will throw an exception
+  Future<void> pumpAndSettle(
+    WidgetsBinding binding, [
+    Duration duration = const Duration(seconds: 10),
+  ]) async {
+    final waitTill = currentDateTime.add(duration);
+    do {
+      if (currentDateTime.isAfter(waitTill)) {
+        throw TimeoutException('pumpAndSettle timed out', duration);
+      }
+      await pump(binding);
+    } while (binding.hasScheduledFrame);
   }
 }
