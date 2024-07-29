05956d02af432fc02b6139853bbb0add83aba7c0
tejHackerDEV <tejasimha222@gmail.com>
2024-07-29 23:08:05 +0530

    feat(driver)!: Initial commit to support running driver in `python`
    Create basic folder structure
    
    Signed-off-by: tejHackerDEV <tejasimha222@gmail.com>

---
+++ packages/driver/python/README.md
@@ -0,0 +1,13 @@
+# My Package
+
+A brief description of my package.
+
+## Installation
+
+To get started with **MyPackage**, you'll need to set up your development environment using `pipenv`. Follow the instructions below to install the package and its dependencies.
+
+### Prerequisites
+
+- Python 3.10 or higher
+- `pip` (Python package installer)
+- `pipenv` (Python dependency manager)
---
+++ packages/driver/python/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flutterenium',
+    version='0.1.0',  # Initial release version
+    description='A brief description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/tejHackerDEV/flutterenium',
+    author='TejHackerDEV',
+    author_email='tejasimha222@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        # List your package dependencies here
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
