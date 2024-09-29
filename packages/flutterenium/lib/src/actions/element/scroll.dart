3d352589d995d8a1d17a57d3757e6a42da86b8cd
tejHackerDEV <tejasimha222@gmail.com>
2024-09-29 16:09:49 +0530

    refactor(flutterenium)!: Remove unnecessary soundness to find `ScrollableState` for scrolling an element
    Signed-off-by: tejHackerDEV <tejasimha222@gmail.com>

--- packages/flutterenium/lib/src/actions/element/scroll.dart
+++ packages/flutterenium/lib/src/actions/element/scroll.dart
@@ -9,8 +9,7 @@ const _kDebugLabel = 'FluttereniumScrollController';
 
 class ScrollAction extends ElementAction {
   const ScrollAction(this.delta, this.milliseconds)
-      : _scrollableFinder = const FindByWidget(),
-        _rawGuestureDetectorFinder = const FindByWidget();
+      : _scrollableFinder = const FindByWidget();
 
   factory ScrollAction.fromJson(Map<String, dynamic> json) {
     return ScrollAction(json['delta'], json['milliseconds']);
@@ -19,38 +18,15 @@ class ScrollAction extends ElementAction {
   final double delta;
   final int? milliseconds;
   final FindByWidget<Scrollable> _scrollableFinder;
-  final FindByWidget<RawGestureDetector> _rawGuestureDetectorFinder;
 
   @override
   FutureOr<bool> execute(WidgetsBinding binding, Element element) async {
     bool didSucceeded = false;
-    // All the code that is written is based on the comments written
-    // by Flutter team inside `Scrollable` widget `build` method.
-    //
-    // So based on the usage of `_ScrollableScope` usage inside
-    // `Scrollable` widget this is developed. If flutter teams changes its
-    // implementation then we need to change & adjust the code accordingly
     final scrollableElement = _scrollableFinder.execute(binding, root: element);
+
     if (scrollableElement != null) {
-      GlobalKey? globalKey;
-      Element? elementForSearch = scrollableElement;
-      do {
-        // as Flutter team uses `RawGuestureDetector` key to define
-        // `notificationContext` we recursively find that widget
-        // till we found the `GlobalKey` & use the that context
-        elementForSearch = _rawGuestureDetectorFinder.execute(
-          binding,
-          root: elementForSearch,
-        );
-        final widget = elementForSearch?.widget;
-        if (widget != null) {
-          final key = widget.key;
-          if (key is GlobalKey) {
-            globalKey = key;
-          }
-        }
-      } while (globalKey == null);
-      final scrollableState = Scrollable.of(globalKey.currentState!.context);
+      final scrollableState =
+          (scrollableElement as StatefulElement).state as ScrollableState;
       // 1.Create our own `ScrollController`
       //
       // 2. Attach the `position` we got from
