c365d12fd469ac79d0cc3f024b0ec50f06be3dfe
tejHackerDEV <tejasimha222@gmail.com>
2024-09-29 17:32:48 +0530

    feat(driver): Add support to find an element via
    Signed-off-by: tejHackerDEV <tejasimha222@gmail.com>

--- packages/driver/lib/finder.py
+++ packages/driver/lib/finder.py
@@ -8,10 +8,11 @@ class FinderKind(Enum):
     LABEL = "label"
     TEXT = "text"
     SVG = "svg"
+    ICON = "icon"
 
 
 class By:
-    def __init__(self, value: str, kind: FinderKind):
+    def __init__(self, value: str | int, kind: FinderKind):
         """Initializes the By class with a value and a finder type.
 
         Args:
@@ -59,6 +60,19 @@ class By:
             By: instance with SVG FinderKind.
         """
         return cls(value=text, kind=FinderKind.SVG)
+
+    @classmethod
+    def icon(cls, value: int) -> "By":
+        """
+        Factory method to create a By instance using icon.
+
+        Args:
+            value (int): The CharCode to find the icon element.
+
+        Returns:
+            By: instance with ICON FinderKind.
+        """
+        return cls(value=value, kind=FinderKind.ICON)
 
     def _to_action(self):
         name = self.kind.value
